0086801b26bcc72ff6eea210d608de124c2f04a9
malcolmclancy <malcolm.clancy@hotmail.com>
2025-03-13 20:16:52 +1000

    add parent node and convert text to html func

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-
-
 class HTMLNode:
     def __init__(self,
                  tag=None,
@@ -38,3 +36,20 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        nodes = ""
+        for child in self.children:
+            nodes += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{nodes}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
